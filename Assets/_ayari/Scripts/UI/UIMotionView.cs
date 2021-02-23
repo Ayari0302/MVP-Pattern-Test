01c560ec6551d5f84a3072d96fd06649d5b60d6d
ayari0302 <ayasuzu0302@gmail.com>
2021-02-23 22:02:57 +0900

    マウスホバーでスライダーの表示/非表示処理を仮作成

--- Assets/_ayari/Scripts/UI/UIMotionView.cs
+++ Assets/_ayari/Scripts/UI/UIMotionView.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class UIMotionView : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private RawImage _hoverImage;
+    [SerializeField] private Slider _slider;
+
+    private void Awake()
     {
-        
+        // ObjectにEventTriggerコンポーネントを追加する
+        var trigger = _hoverImage.gameObject.AddComponent<EventTrigger>();
+
+        // 登録するイベントを設定する
+        var entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerEnter};
+
+        entry.callback.AddListener((data) =>
+        {
+            ShowSlider();
+            Debug.Log("PointerEnter");
+        });
+
+        trigger.triggers.Add(entry);
+
+        entry = new EventTrigger.Entry {eventID = EventTriggerType.PointerExit};
+        entry.callback.AddListener((data) =>
+        {
+            HideSlider();
+            Debug.Log("PointerExit");
+        });
+        trigger.triggers.Add(entry);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideSlider()
     {
-        
+        // スライダーを非表示にする
+        _slider.gameObject.SetActive(false);
+    }
+
+    private void ShowSlider()
+    {
+        // スライダーを表示する
+        _slider.gameObject.SetActive(true);
     }
 }
